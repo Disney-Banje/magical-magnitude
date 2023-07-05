d7f3767eb77e41a12381b28f107cd851483aca2c
Disney <disneyconstant996@gmail.com>
2023-07-05 20:49:27 +0300

    add extra styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'Alata': ['Alata', 'sans-serif'],
+			'Actor': ['Actor', 'sans-serif'],
+		},
+		backgroundImage: {
+			'pattern': "url('../images/background.svg')",
+		},
 	},
 	plugins: [],
 }
